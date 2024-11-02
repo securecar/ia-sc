ec00453dc1e332e29900b7d191b0909cb37a99e2
gustavodscruz <gustavodiasdsc@gmail.com>
2024-11-02 16:11:34 -0300

    funções para mandar para o banco o conserto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 
 # %%
+import oracledb as db
+from flask import Flask, request, jsonify
 import pickle
 import re
 
@@ -13,15 +15,19 @@ with open('modelo_causas_aprimorado.pkl', 'rb') as f:
     mlb = data['mlb']
 
 # Função para normalizar o texto
+
+
 def normalizar_texto(texto):
     return texto.strip().lower()
 
 # Função para fazer predições
+
+
 def prever_causas(problema_input, quilometragem_input):
     # Normalizar entradas
     problema_input_norm = normalizar_texto(problema_input)
     quilometragem_input_norm = normalizar_texto(quilometragem_input)
-    
+
     # Criar a entrada combinada
     entrada = problema_input_norm + ' ' + quilometragem_input_norm
 
@@ -36,7 +42,8 @@ def prever_causas(problema_input, quilometragem_input):
 
     # Verificar se há causas previstas
     if causas_previstas and causas_previstas[0]:
-        print(f"Possíveis causas para '{problema_input}' com quilometragem '{quilometragem_input}':")
+        print(f"Possíveis causas para '{
+              problema_input}' com quilometragem '{quilometragem_input}':")
         for causa in causas_previstas[0]:
             print(f"- {causa}")
         return f"Possíveis causas para {str(problema_input).lower()} com quilometragem {quilometragem_input} são: {', '.join(causas_previstas[0])} "
@@ -44,16 +51,17 @@ def prever_causas(problema_input, quilometragem_input):
         return f"Nenhuma causa prevista para '{problema_input}' com quilometragem '{quilometragem_input}'."
         # print(f"Nenhuma causa prevista para '{problema_input}' com quilometragem '{quilometragem_input}'.")
 
-import oracledb as db
 
 # def inserir_conserto():
 # fazer pdf
+
 
 def get_conexao():
     LOGIN = "rm556448"
     PWD = "fiap24"
     return db.connect(user=LOGIN,
-                            password=PWD, dsn='oracle.fiap.com.br/orcl')
+                      password=PWD, dsn='oracle.fiap.com.br/orcl')
+
 
 def pecas():
     sql = "SELECT ds_peca, id_peca FROM t_securecar_peca"
@@ -66,15 +74,14 @@ def pecas():
 
 
 def buscaCPF(cpf):
+    sql = "select id_usuario, nm_usuario from t_securecar_usuario where nr_cpf like :cpf"
 
-
-    sql = "select id_usuario from t_securecar_usuario where cpf like :cpf"
-    
     with get_conexao() as con:
         with con.cursor() as cur:
-            cur.execute(sql, {"cpf" : cpf})
+            cur.execute(sql, {"cpf": cpf})
             user_id = cur.fetchone()
     return user_id
+
 
 def montar_dicionario_pecas(resultado, pecas_dict):
     resultado_lower = resultado.lower()
@@ -88,71 +95,71 @@ def montar_dicionario_pecas(resultado, pecas_dict):
             peca_counts[peca_id] = count
     return peca_counts
 
+def inserir_conserto(resultado, pecas, user_id):
+    sql = f"insert into t_securecar_conserto(dt_conserto, ds_conserto, vl_conserto, id_usuario) values (to_date(SYSDATE, 'DD-MM-YYYY'), :resultado, 0, :user_id) returning id_conserto into :id"
 
+    input = {
+        "resultado" : resultado,
+        "user_id" : user_id
+    }
+    with get_conexao() as con:
+        with con.cursor() as cur:
+            cur.execute(sql, input)
+        con.commit()
+    return input['id']
+        
 
-from flask import Flask, request, jsonify
+def inserir_peca_conserto(id_conserto, pecas_encontradas: dict):
+    sql = "insert into t_securecar_peca_conserto(id_conserto, id_peca) values(:id_conserto, :id_peca)"
+    dados = []
+    for id_peca, quantidade in pecas_encontradas.items():
+        for _ in range(quantidade):
+            dados.append({"id_conserto": id_conserto, "id_peca": id_peca})
+
+    with get_conexao() as con:
+        with con.cursor() as cur:
+            cur.executemany(sql, dados)
+        con.commit()
+
 
 app = Flask(__name__)
-
 
 
 @app.route('/prever', methods=['POST'])
 def prever():
     data = request.get_json()
-    
-    # Verificar se os dados recebidos são um dicionário
-    if not isinstance(data, dict):
-        return jsonify({"error": "Dados inválidos. Envie um JSON válido."}), 400
-    
-    # Obter o campo 'cpf'
-    cpf = str(data.get('cpf', '')).strip()
-    
-    # Validar se 'cpf' foi fornecido
-    if not cpf:
-        return jsonify({"error": "O campo 'cpf' é obrigatório."}), 400
-    
-    # Remover '-', '/', '.' do CPF
-    cpf_cleaned = re.sub(r'[-./]', '', cpf)
-    
-    # Validar se CPF contém exatamente 11 dígitos
-    if not re.fullmatch(r'\d{11}', cpf_cleaned):
-        return jsonify({"error": "CPF inválido. Por favor, forneça um CPF com 11 dígitos."}), 400
-    
-    # Buscar id_usuario usando CPF limpo
-    user_id = buscaCPF(cpf_cleaned)
-    if not user_id:
-        return jsonify({"error": "Usuário não encontrado para o CPF fornecido."}), 404
-    
-    # Verificar se apenas 'cpf' foi fornecido
-    keys = data.keys()
-    if len(keys) == 1:
-        # Apenas 'cpf' foi fornecido, retornar apenas o 'user_id'
+
+    if str(data.get('type', '')).strip() == 'cpf':
+        cpf = str(data.get('cpf', '')).strip()
+        cpf_cleaned = re.sub(r'[-./]', '', cpf)
+        user = buscaCPF(cpf_cleaned)
+        if not user:
+            return jsonify({"error": "Usuário não encontrado para o CPF fornecido."}), 404
         return jsonify({
-            "user_id": user_id[0]  # Assumindo que user_id é uma tupla e queremos o primeiro elemento
+            "id": user[0],
+            "username": user[1]
         })
-    
-    # Se mais campos forem fornecidos, verificar se 'problema' e 'quilometragem' estão presentes
-    problema = str(data.get('problema', '')).strip()
-    quilometragem = str(data.get('quilometragem', '')).strip()
-    
-    # Validar se 'problema' e 'quilometragem' foram fornecidos
-    if not problema or not quilometragem:
-        return jsonify({"error": "Os campos 'problema' e 'quilometragem' são obrigatórios quando fornecidos."}), 400
-    
-    # Adicionar 'km' à quilometragem
-    quilometragem_km = quilometragem + 'km'
-    
-    # Fazer a previsão das causas
-    resultado = prever_causas(problema, quilometragem_km)
-    
-    # Montar o dicionário de peças e suas quantidades (apenas as encontradas no resultado)
-    pecas_encontradas = montar_dicionario_pecas(resultado, lista_pecas)
-    
-    return jsonify({
-        "user_id": user_id[0],  # Assumindo que user_id é uma tupla e queremos o primeiro elemento
-        "text": resultado,
-        "pecas": pecas_encontradas
-    })
+    else:
+        problema = str(data.get('problema', '')).strip()
+        quilometragem = str(data.get('quilometragem', '')).strip()
+        id_usuario = int(data.get('user', ''))
+        if not problema or not quilometragem:
+            return jsonify({"error": "Os campos 'problema' e 'quilometragem' são obrigatórios quando fornecidos."}), 400
+
+        quilometragem_km = quilometragem + 'km'
+        resultado = prever_causas(problema, quilometragem_km)
+        lista_pecas = pecas()
+        pecas_encontradas = montar_dicionario_pecas(resultado, lista_pecas)
+        id_conserto = inserir_conserto(resultado=resultado, pecas=pecas, user_id=id_usuario)
+        inserir_peca_conserto(id_conserto=id_conserto, pecas_encontradas=pecas_encontradas)
+        
+        print(pecas_encontradas)
+        
+        return jsonify({
+            "text": resultado,
+        })
+
+   
 
 if __name__ == "__main__":
     app.run()
@@ -160,6 +167,3 @@ if __name__ == "__main__":
 # Exemplo de uso com valores presentes no dataset
 # problema = 'Barulho no motor'
 # quilometragem = '50.000km'
-
-
-
